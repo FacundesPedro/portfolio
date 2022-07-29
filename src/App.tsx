edb64197f52a0408a4035958f148f2b292934dc4
FacundesPedro <pedro.f.facundes1@gmail.com>
2022-07-29 16:20:22 -0300

    [algo]

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,12 @@ function App() {
         ({state,fullpageApi}) => (
           <ReactFullpage.Wrapper>
             <SectionWrapper styles={{backgroundColor:'blue'}}>
-              <h2>section 1</h2>
+              <div style={{height:'100%',padding:'0 8em' ,display:'flex', alignItems:'center',justifyContent:'center'}}>
+                <img width={'30%'} style={{paddingRight:'2em'}} src="https://raw.githubusercontent.com/FacundesPedro/FacundesPedro/main/png_to_gif.gif" alt="Facunde's Pedro GitHub-Icon" />
+                <div style={{flex:1}}>
+                 {' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates dicta quae aut odio magni commodi repudiandae tempora ullam quasi, corporis possimus fugit, placeat natus laboriosam maiores ducimus quo enim. Aut.'}
+                </div>              
+              </div>
             </SectionWrapper>
             <div className="section">
               <h2>section 2</h2>
